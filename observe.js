b9ea1e36415fe95bd7f4c7181ccf255c4d502fad
furyzhao <furyzhao@tencent.com>
2023-08-18 13:13:34 +0800

    fix: 数据监听

---
+++ observe.js
@@ -0,0 +1,19 @@
+function observe(data, callback) {
+  return new Proxy(data, {
+    set(target, key, value, receiver) {
+      const result = Reflect.set(target, key, value, receiver);
+      if (result) {
+        callback(key, value);
+      }
+      return result;
+    },
+  });
+}
+
+// 使用方法
+const data = observe({ name: "张三", age: 20 }, (key, value) => {
+  console.log(`属性${key}的值变为${value}`);
+});
+
+data.name = "李四"; // 输出：属性name的值变为李四
+data.age = 22; // 输出：属性age的值变为22
