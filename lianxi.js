d32734aa5275ca658f14dceef49cacd13d8f1b3f
furyzhao <furyzhao@tencent.com>
2023-09-02 11:12:18 +0800

    fxi:

--- lianxi.js
+++ lianxi.js
@@ -1,13 +1,28 @@
-function classifyStrings(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const key = str.split("").sort().join("");
-    if (map.has(key)) {
-      map.get(key).add(str);
-    } else {
-      map.set(key, new Set([str]));
+function countWaysToSplit(arr) {
+  let count = 0;
+
+  function backtrack(start, group, product) {
+    if (group === 3) {
+      if (product > 1) count++;
+      return;
+    }
+
+    for (let i = start; i < arr.length; i++) {
+      // 将当前元素加入到当前组，并更新乘积
+      const newProduct = product * arr[i];
+      if (newProduct > 1) {
+        // 如果当前组的乘积大于1，继续搜索下一组
+        backtrack(i + 1, group + 1, newProduct);
+      }
     }
   }
-  return map.size;
+
+  // 从数组的第一个元素开始搜索，当前处于第1组
+  backtrack(0, 1, 1);
+
+  return count;
 }
-console.log(classifyStrings(['abc','bcaa']));
+
+// 示例
+const arr = [2, 3, 4, 5, 6];
+console.log(countWaysToSplit(arr)); // 输出：10
